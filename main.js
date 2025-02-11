8b44657003ed1b0f33d7a3bd13d254bdebdb147a
julia-martynovych <inquam414@gmail.com>
2025-02-11 10:54:54 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+//Clases
+
+class Game {
+    costructor() {
+        this.container = document.getElementById("game-container");
+        this.personaje = null;
+
+    } 
+    crearEscenario() {
+        
+    }
+    agregarEventos() {
+        
+    }
+    checkColisiones() {
+        
+    }
+}
+class Personaje {
+
+}
+class Moneda {
+
+}
+const juego = new Game()
