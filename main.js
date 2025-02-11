b5a0c9155546ad49bbeb1fab1d3c17377022e5f8
julia-martynovych <inquam414@gmail.com>
2025-02-11 11:28:28 +0100

    created class heroe

--- main.js
+++ main.js
@@ -4,19 +4,61 @@ class Game {
     costructor() {
         this.container = document.getElementById("game-container");
         this.personaje = null;
+        this.monedas = [];
+        this.puntuacion = 0;
+        this.crearEscenario();
+        this.agregarEventos();
 
     } 
     crearEscenario() {
-        
+        this.personaje = new Personaje();
+        this.container.appendChild(this.personaje.element);
+        for (let i = 0; i < 5; i++){
+            const moneda = new Moneda();
+            this.monedas.push(moneda);
+            this.container.appendChild(moneda.element);
+        }
+
     }
     agregarEventos() {
-        
+        window.addEventListener("keydown", (e) => this.personaje.mover(e));
+        this.checkColisiones();
     }
     checkColisiones() {
-        
+        setInterval(() => {
+            this.monedas.forEach((moneda, index) => {
+                if (this.personaje.colisionaCon(moneda)) {
+                    this.container.removeChild(moneda.element);
+                    this.monedas.splice(index, 1);
+                }
+            })
+        },
+        100)
     }
 }
 class Personaje {
+    constructor() {
+        this.x = 50;
+        this.y = 300;
+        this.width = 50;
+        this.height = 50;
+        
+    }
+    mover() {
+        
+    }
+    saltar() {
+        
+    }
+    caer() {
+        
+    }
+    actualizarPosicion() {
+        
+    }
+    colisionaCon() {
+        
+    }
 
 }
 class Moneda {
